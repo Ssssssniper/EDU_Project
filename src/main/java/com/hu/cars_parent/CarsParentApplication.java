f9fcc857c617e6178a0b0bad1168a67a04ed6bb0
Hu <huqizheng2631995@163.com>
2020-07-08 14:16:17 +0800

    day1

---
+++ src/main/java/com/hu/cars_parent/CarsParentApplication.java
@@ -0,0 +1,13 @@
+package com.hu.cars_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hu/cars_parent/CarsParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.cars_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
